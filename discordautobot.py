b724e1b82f45ac92c6dba0b9ad4afb682cfaafee
KatLuLzSeC <94811442+KatLuLzSeC@users.noreply.github.com>
2021-12-04 16:40:50 -0500

    Add files via upload

---
+++ discordautobot.py
@@ -0,0 +1,10 @@
+import keyboard
+import pyautogui
+c = True
+while c:
+    if keyboard.is_pressed('q'): break #click q to break operation
+    pyautogui.click(47,167) 
+    pyautogui.click(358,381)
+    pyautogui.click(867,546)
+    pyautogui.click(862,586)
+    pyautogui.click(608,681) #edit this value to ur correct value
